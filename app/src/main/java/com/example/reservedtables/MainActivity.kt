58ec54186daf0080216bb9ec2ae425d5ecab5f6a
maincorename <turbocatrom@gmail.com>
2019-07-02 23:56:04 +0700

    first push

---
+++ app/src/main/java/com/example/reservedtables/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.reservedtables
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener{
+            val intent = Intent(this, reserve::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/reservedtables/reserve.kt
@@ -0,0 +1,13 @@
+package com.example.reservedtables
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class reserve : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserve)
+    }
+
+}

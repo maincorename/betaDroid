e46238a194d4541584622fbf214c65e45e9798f2
Hutlscot <vorontsov.r@bk.ru>
2019-07-03 16:47:36 +0700

    карта столов

--- app/src/main/java/com/example/reservedtables/MainActivity.kt
+++ app/src/main/java/com/example/reservedtables/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.reservedtables
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,4 +15,8 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+    fun click(view: View) {
+        val map = Intent(this, reserve::class.java)
+        startActivity(map)
+    }
     }
